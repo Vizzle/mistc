62c0052b3b37fc1a417c18c16de7485412d5e85b
pangza.lgl <pangza.lgl@koubei.com>
2019-10-16 00:02:55 +0800

    增加测试用例

--- test/index.js
+++ test/index.js
@@ -3,11 +3,29 @@ const fs = require('fs-extra')
 const path = require('path')
 const { compile } = require('../out')
 
-it('compile', async () => {
-  const cwd = process.cwd()
-  const inputFile = path.resolve(cwd, 'test/fixtures/test.mist')
-  const outputFile = path.resolve(cwd, 'test/fixtures/output.mist')
-  const output = JSON.parse(await compile(inputFile))
-  const expectedOutput = JSON.parse(await fs.readFile(outputFile, 'utf-8'))
-  assert.deepEqual(output, expectedOutput, '编译结果与预期不符')
+function expectOutput(name) {
+  it(name, async () => {
+    const cwd = process.cwd()
+    const inputFile = path.resolve(cwd, `test/fixtures/${name}.mist`)
+    const outputFile = path.resolve(cwd, `test/fixtures/${name}_out.mist`)
+    const output = JSON.parse(await compile(inputFile))
+    const expectedOutput = JSON.parse(await fs.readFile(outputFile, 'utf-8'))
+    assert.deepEqual(output, expectedOutput, '编译结果与预期不符')
+  })
+}
+
+function expectThrows(name, error) {
+  it(name, async () => {
+    const cwd = process.cwd()
+    const inputFile = path.resolve(cwd, `test/fixtures/${name}.mist`)
+    assert.rejects(() => compile(inputFile), error)
+  })
+}
+
+describe('Tests', () => {
+  expectOutput('basic')
+  expectOutput('nested')
+  expectOutput('import_in_slot')
+  expectOutput('repeat_slot')
+  expectThrows('circular_throws', '不允许组件循环引用 a')
 })
